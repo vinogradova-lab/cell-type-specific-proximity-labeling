f49a069dd534a2410c61f1abc44476894c849e97
Nathalie Ropek <nropek@rockefeller.edu>
2023-03-22 10:37:35 -0400

    cleans up repo

--- tiap/tiap/__main__.py
+++
@@ -1,8 +0,0 @@
-
-# -*- coding: utf-8 -*-
-
-"""jcast.__main__: executed when jcast directory is called as script."""
-
-from tiap.main import main
-
-main()
--- uniprot_noteboook/not relevant/biomart_r.R
+++
@@ -1,5 +0,0 @@
-require(biomaRt)
-
-
-
-mart<-useMart(biomart = “ensembl”, dataset =  “mmusculus_gene_ensembl”)mart <- useDataset(dataset=”mmusculus_gene_ensembl”, mart=mart)mapping <-  getBM(attributes=c(“mgi_symbol”,”ensembl_gene_id”,”entrezgene_id”), filters = “mgi_symbol”, mart=mart, values=data, uniqueRows=TRUE, bmHeader = T)
