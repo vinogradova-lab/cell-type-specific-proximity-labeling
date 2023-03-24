04116bb39b77deaf6fefb7471cbc104837ff3e20
Nathalie Ropek <nropek@rockefeller.edu>
2023-03-24 18:23:51 -0400

    moves fasta funcs into separate file

---
+++ src/fasta_table_funcs.py
@@ -0,0 +1,36 @@
+def remove_duplicates_from_aggregate(row): 
+    list_of_cols = row.index.tolist()
+    for col in list_of_cols: 
+        row[col] = ', '.join(map(str, set(row[col])))
+    return row
+
+
+def annotate_TP(subcelullar_location):
+    tp_term_list = ["Endoplasmic Reticulum", "Secreted", "secreted", "Endoplasmic reticulum", "Rough endoplasmic reticulum", "endoplasmic reticulum"]
+    # "Golgi", "Extracellular space", "extracellular space", 
+    subcelullar_location = str(subcelullar_location)
+    if "Note:" in subcelullar_location:
+        subcelullar_location = subcelullar_location.split("Note:")[0]
+    for item in tp_term_list: 
+        if item in subcelullar_location:
+            return "TP" 
+    
+
+def annotate_FP(subcelullar_location):
+    fp_term_list = ["Mitochondrion", "mitochondrion"] # 'cytoskeleton' "Cytoskeleton", "Nucleus", "nucleus", 
+    subcelullar_location = str(subcelullar_location)
+    if "Note:" in subcelullar_location:
+        subcelullar_location = subcelullar_location.split("Note:")[0]
+    for item in fp_term_list: 
+        if item in subcelullar_location:
+            return "FP" 
+
+
+def annotation(df):   
+    if df["TP"] == "TP":
+        return "TP" 
+    # if signal peptide is present then even though annotation says FP, change annotation to nan
+    elif df["FP"] == "FP" and pd.notna(df["Signal peptide"]):
+        return np.nan
+    elif df["FP"] == "FP" and pd.isnull(df["Signal peptide"]):
+        return "FP"
